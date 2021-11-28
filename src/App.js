652b04693ef46dd35e7f6cbb1c1e47b4ee5ea210
Srutip04 <subuhishruti75@gmail.com>
2021-11-28 20:10:26 +0530

    storing user data in firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component";
-import {auth} from './firebase/firebase.utils';
+import {auth , createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends Component {
@@ -20,8 +20,9 @@ class App extends Component {
    unsubscribeFromAuth = null
 
    componentDidMount(){
-     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-       this.setState({currentUser: user});
+     this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
+      //  this.setState({currentUser: user});
+      createUserProfileDocument(user);
        console.log(user);
      })
    }

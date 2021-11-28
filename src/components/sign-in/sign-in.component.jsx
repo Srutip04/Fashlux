9194672c9af2eb7b60c770b23b6d2d1d02e5e07e
Srutip04 <subuhishruti75@gmail.com>
2021-11-29 02:48:25 +0530

    type

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -44,7 +44,7 @@ class SignIn extends Component{
                     <FormInput name='password' type='password' value={this.state.password} onChange={this.handleChange} label='password' required />
                     <div className='buttons'>
                     <CustomButton type='submit'  >Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
+                    <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>Sign In with Google</CustomButton>
                     </div>
                 </form>
             </div>
